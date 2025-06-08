a0903535bed6120b6990bf63b56c16c5f84cf425
007Edge <edge07.shiv@gmail.com>
2025-06-08 12:41:36 +0530

    added some comments

--- revesion/Strings.php
+++ revesion/Strings.php
@@ -11,18 +11,20 @@
     echo strrev($s)."<br>";
     echo trim($s)."<br>";   // remove white space from start & end
 
-    $arr = explode(" ", $s);
+    $arr = explode(" ", $s);  // convert string into array
 
     print_r($arr); // concatinate with . operator
     echo "<br>";
 
-    echo substr($s, 10,2)."<br>";
+    echo substr($s, 10,2)."<br>"; 
+    // from 10th position of size 2 substring will be retrived
     echo substr($s, 6)."<br>";
+    // from 6th to last
     echo substr($s, -7,4)."<br>";
+    // from -7 idx (negitive idx start from last) right of size 4
 
 
 
     
-
 
 ?>
